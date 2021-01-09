bf8d828ea9c2b68707cf0dd6e361b40f4215beb9
Mirte Golverdingen <mmgolverdingen@gmail.com>
2021-01-09 18:06:50 +0100

    BLAST pairs: remove the data_blast, include individual and all sequence blast in function

--- BLAST_pairs.py
+++ BLAST_pairs.py
@@ -1,56 +1,24 @@
 from Bio.Blast import NCBIWWW
-from Bio.Blast import NCBIXML
-
-# #individual sequences blast
 from Bio import SeqIO
 
-sequence_dict = SeqIO.to_dict(SeqIO.parse('fasta_sequences.fasta', 'fasta'))
-
-
-# for name, sequence in sequence_dict.items():
-#     blast_name = './Fasta/' + name +'.xml'
-#     result_handle = NCBIWWW.qblast('blastn', 'nt', sequence.format("fasta"))
-#     with open(blast_name, "w") as save_to:
-#         save_to.write(result_handle.read())
+def individual_sequence_blast(filename, species):
+    sequence_dict = SeqIO.to_dict(SeqIO.parse(filename, 'fasta'))
+       
+    for name, sequence in sequence_dict.items():
+        blast_name = './Fasta/' + name +'.xml'
+        result_handle = NCBIWWW.qblast('blastn', 'nt', sequence.format("fasta"))
+        with open(blast_name, "w") as save_to:
+            save_to.write(result_handle.read())
             
         
 #all sequences blast  
-species = 'Treponema pallidum subsp. pallidum'  
-query = 'nuccore pubmed[filter] NOT '+species+'[Organism]'    
-sequence_data = open('fasta_sequences.fasta')
-result_handle = NCBIWWW.qblast('blastn', 'nt', sequence_data.read(), entrez_query=query)
-sequence_data.close()
-
-with open("fasta_data.xml", "w") as save_to:
-    save_to.write(result_handle.read())
-    result_handle.close()
-
-result_handle = open("fasta_data.xml", 'r')
-blast_records = NCBIXML.parse(result_handle)
-# blast_record = next(blast_records)
-species = 'Treponema pallidum subsp. pallidum'
-
-#Processing Blast Data 
-blast_save = {}
-for sequence in blast_records:
-    E_VALUE_THRESH = 1
-    unique = True
-    for alignment in sequence.alignments:
-        for hsp in alignment.hsps:
-            title = str(alignment.title)
-            if not hsp.expect < E_VALUE_THRESH:
-                unique = False
-    if unique:
-       blast_save[sequence.query] = [alignment.title, hsp.query[0:23]]
-
-
-# for title, sequence in blast_save.items():
-#     if sequence == 'CCTGGTGTTGGTTACCGGCGTCG':  
-#         print('yes')     
-#         print('title')
-##
-# print(result_matches)
-# print(fasta_seq, fasta_name)
-
-# fasta_try = 'Example of a single sequence in FASTA/Pearson format:\n\n\n> sequence A\n' + fasta_try[1] + '\n\n'
-# result_handle = NCBIWWW.qblast('blastn','nt', fasta_try)
+def blast_all_sequences(fasta_filename, species, xml_filename):
+    # query = 'nuccore pubmed[filter] NOT '+species+'[Organism]'
+    query = '(none)'    
+    sequence_data = open(fasta_filename)
+    result_handle = NCBIWWW.qblast('blastn', 'nt', sequence_data.read(),entrez_query=query)
+    sequence_data.close()
+    
+    with open(xml_filename, "w") as save_to:
+        save_to.write(result_handle.read())
+        result_handle.close()
